6910ea001fd7154f3f44b03b419778b61d96d4d4
SimonDdraig <simon.ddraig@gmail.com>
2025-07-25 15:18:26 +0800

    strands - cloud9 config

--- bedrock/strands-crypto/03 multi agent example/config.py
+++ bedrock/strands-crypto/03 multi agent example/config.py
@@ -7,7 +7,9 @@ INFERENCE_MODEL = "amazon.nova-pro-v1:0"
 REGION = "us-east-1"
 
 # Your Bedrock Knowledge Base ID
+# REPLACE THIS WITH YOURS
 KB_ID = "XXXXXXXX"
 
+# DO NOT REPLACE THIS ONE
 if KB_ID == "XXXXXXXX":
     raise RuntimeError("⚠️ Execution stopped: KB_ID is not configured properly.\nFind your Bedrock Knowledge Base ID from a previous lab or in your console, and set in config.py!")
